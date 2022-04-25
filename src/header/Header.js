de5ba9ada18b7ae1dba1cdd3288ee55ed0a3327c
Sarthak Ladhwe <sarthak.ladhwe156@gmail.com>
2022-04-25 17:26:01 +0530

    Header styling, adjusted few sylings

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import './Header.css'
+import heroImage from '../assets/SpaceTimeTalks-Hero.jpg'
+import logoImage from '../assets/STTWebsiteWhiteLogo.png'
 
 export default function Header() {
   return (
     <div className='header'>
-        Header
+        <div className='header-titles'>
+            <img className='logo-image' src={logoImage} alt='logo-image' />
+            <span className='header-title-large'>Blog</span>
+        </div>
+        <img className='hero-image' src={heroImage} alt='hero-image' />
     </div>
   )
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,6 +7,7 @@ export default function Home() {
   return (
     <div className='home'>
         <Header />
+        
     </div>
   )
 }
