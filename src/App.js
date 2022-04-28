7c6605ef7496684662a256d087a5dc54148e077f
Sarthak Ladhwe <sarthak.ladhwe156@gmail.com>
2022-04-28 20:38:32 +0530

    Added Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import Topbar from "./components/topbar/Topbar";
+import Home from "./pages/Home/Home"
+import Write from "./pages/Write/Write"
+import Settings from "./pages/Settings/Settings"
+import Single from "./pages/Single/Single"
+import Login from "./pages/Login/Login"
 import Register from "./pages/Register/Register";
 
+import { Routes, Route} from "react-router-dom";
+
 function App() {
+  const user = true
   return (
     <div className="App">
-      {/* <Topbar /> */}
-      <Register />
+      <Topbar />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/write" element={user ? <Write /> : <Register />} />
+        <Route path="/settings" element={user ? <Settings /> : <Register />} />
+        <Route path="/post/:postId" element={<Single />} />
+        <Route path="/register" element={user ? <Home/> : <Register />} />
+        <Route path="/login" element={user ? <Home/> : <Login />} />
+      </Routes>
     </div>
   );
 }
--- src/components/topbar/Topbar.js
+++ src/components/topbar/Topbar.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './topbar.css'
 
+import { Link } from 'react-router-dom'
+
 export default function Topbar() {
   return (
     <div className='topbar'>
@@ -11,15 +13,23 @@ export default function Topbar() {
         </div>
         <div className='top-center'>
             <ul className='menu-list'>
-                <li className='menu-list-item'>HOME</li>
+                <Link to="/">
+                    <li className='menu-list-item'>HOME</li>
+                </Link>
+                <Link to="/write">
+                    <li className='menu-list-item'>WRITE</li>
+                </Link>
+                
                 <li className='menu-list-item'>ABOUT</li>
-                <li className='menu-list-item'>WRITE</li>
+                
                 <li className='menu-list-item'>CONTACT</li>
                 <li className='menu-list-item'>LOGOUT</li>
             </ul>
         </div>
         <div className='top-right'>
-            <img src='https://media-exp1.licdn.com/dms/image/C4D03AQEk3saQzwZMEw/profile-displayphoto-shrink_800_800/0/1583911954522?e=1656547200&v=beta&t=eumbJSoMV76Bx3RFd5EC7PBCuHJQGGjp7zM1zzJqv3U' className='profile-image' alt='' />
+            <Link to="/settings">
+                <img src='https://media-exp1.licdn.com/dms/image/C4D03AQEk3saQzwZMEw/profile-displayphoto-shrink_800_800/0/1583911954522?e=1656547200&v=beta&t=eumbJSoMV76Bx3RFd5EC7PBCuHJQGGjp7zM1zzJqv3U' className='profile-image' alt='' />
+            </Link>
             <i className="top-search fa-solid fa-magnifying-glass"></i>
         </div>
     </div>
