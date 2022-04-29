cede490c0002faac16499c8a57e5ab34e06072cd
Sarthak Ladhwe <sarthak.ladhwe156@gmail.com>
2022-04-29 18:23:50 +0530

    Installed express mongoose dotenv multer

---
+++ client/src/pages/Register/Register.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './register.css'
+import { Link } from 'react-router-dom'
+
+export default function Register() {
+  return (
+    <div className='register'>
+        <span className='register-title'>Register</span>
+        <form className='register-form'>
+            <label>Username</label>
+            <input type="text" className='register-input' placeholder='Enter Username' />
+            <label>Email</label>
+            <input type="email" className='register-input' placeholder='Enter Email' />
+            <label>Password</label>
+            <input type="password" className='register-input' placeholder='Enter Password' />
+            <button className='register-register'>
+              <Link className='link' to="/register">
+                Register
+              </Link>
+            </button> 
+            <button className='login-button'>
+              <Link className='link' to="/login">
+                I'll login
+              </Link>
+            </button>
+        </form>
+    </div>
+  )
+}
