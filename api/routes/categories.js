e3fa8b420bd75528a229ca061efb814223ebd1fc
Sarthak Ladhwe <sarthak.ladhwe156@gmail.com>
2022-04-30 19:08:22 +0530

    File upload API

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -13,4 +13,13 @@ router.post("/", async (req, res) => {
     }
 })
 
+router.get("/", async (req, res) => {
+    try {
+        const categories = await Category.find()
+        res.status(200).json(categories);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+})
+
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const multer = require("multer");
 
 const authRoute = require("./routes/auth");
 const usersRoute = require("./routes/users");
@@ -14,6 +15,19 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URL)
 .then(console.log("connected to mongo"))
 .catch(console.log(err => console.log(err)));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images")
+    }, filename: (req, file, cb) => {
+        cb(null, req.body.name)
+    }
+});
+
+const upload = multer({storage});
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(201).json("File has been uploaded")
+})
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
