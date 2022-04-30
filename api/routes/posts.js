f0c513058a77870d3ee91c334969198c157bac25
Sarthak Ladhwe <sarthak.ladhwe156@gmail.com>
2022-04-30 17:50:05 +0530

    Update and Delete post

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -9,7 +9,7 @@ router.post("/", async (req, res) => {
     const newPost = new Post(req.body);
 
     try {
-        const savedPost = newPost.save();
+        const savedPost = await newPost.save();
         res.status(201).json(savedPost); 
     } catch(err) {
         res.status(500).json(err)
@@ -17,31 +17,45 @@ router.post("/", async (req, res) => {
 })
 
 // UPDATE POST
-router.delete("/:id", async (req, res) => {
-    if(req.body.userId === req.params.id) {
-        try {
-            const user = await User.findById(req.params.id);
-            if(user) {
-                try {
-                    await Post.deleteMany({username: user.username})
-                    await User.findByIdAndDelete(req.params.id)
-                    res.status(200).json("User has been deleted.");
-                
-                } catch(err) {
-                    res.status(500).json(err)
-                }
-            }
-        } catch {
-            res.status(104).json("User not found!");
+router.put("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if(post.username === req.body.username) {
+            try {
+                const updatedPost = await Post.findByIdAndUpdate(req.params.id, {
+                    $set: req.body
+                }, {new: true});
+
+                res.status(200).json(updatedPost);
+            } catch(err) {
+                res.status(500).json(err)
+            }       
+        } else {
+            res.status(401).json("You can update only your post!")
         }
-        
-    } else {
-        return res.status(401).json("You can delete only your account!")
+    } catch(err) {
+        res.status(500).json(err)
     }
-    
 })
 
 // DELETE POST
+router.delete("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if(post.username === req.body.username) {
+            try {
+                await Post.findByIdAndDelete(req.params.id);
+                res.status(200).json(`Post '${post.title}' has been deleted.`);
+            } catch(err) {
+                res.status(500).json(err)
+            }       
+        } else {
+            res.status(401).json("You can delete only your post!")
+        }
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
 
 // GET POST
 router.get("/:id", async (req, res) => {
