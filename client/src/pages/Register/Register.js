d8a23ea1356051b6ecf62673b9d9bff1664ddc01
Sarthak Ladhwe <sarthak.ladhwe156@gmail.com>
2022-05-04 12:40:56 +0530

    Register Form useState update

--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -1,18 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './register.css'
 import { Link } from 'react-router-dom'
 
 export default function Register() {
+
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    password: ""
+  });
+
+  function userOnChange(event) {
+    setUser(prevUser => (
+      {
+        ...prevUser,
+        [event.target.name]: event.target.value
+      }
+    ))
+  }
+
+  console.log(user);
+
   return (
     <div className='register'>
         <span className='register-title'>Register</span>
         <form className='register-form'>
             <label>Username</label>
-            <input type="text" className='register-input' placeholder='Enter Username' />
+            <input 
+              type="text" 
+              name='username' 
+              value={user.username}
+              onChange={userOnChange}
+              className='register-input' 
+              placeholder='Enter Username'
+            />
             <label>Email</label>
-            <input type="email" className='register-input' placeholder='Enter Email' />
+            <input 
+              type="email" 
+              name='email' 
+              value={user.email}
+              onChange={userOnChange}
+              className='register-input' 
+              placeholder='Enter Email' 
+            />
             <label>Password</label>
-            <input type="password" className='register-input' placeholder='Enter Password' />
+            <input 
+              type="password" 
+              name='password' 
+              value={user.password}
+              onChange={userOnChange}
+              className='register-input' 
+              placeholder='Enter Password' 
+            />
             <button className='register-register'>
               <Link className='link' to="/register">
                 Register
