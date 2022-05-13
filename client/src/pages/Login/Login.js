173232b8feb032adcf378359f422dbf20ac8c9ee
Sarthak Ladhwe <sarthak.ladhwe156@gmail.com>
2022-05-13 11:22:58 +0530

    Register Login styling

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -11,7 +11,7 @@ export default function Login() {
     password: ""
   })
 
-  const {userDetails, loginWithDetails, error} = React.useContext(Context);
+  const {loginWithDetails, error} = React.useContext(Context);
 
   function onDetailsChange(event) {
     setUserLoginDetails(prevDetails => ({
@@ -27,38 +27,40 @@ export default function Login() {
   }
 
   return (
-    <div className='login'>
-        <span className='login-title'>Login</span>
-        <form className='login-form' onSubmit={onLoginSubmit}>
-            <label>Username</label>
-            <input 
-              type="text" 
-              name="username" 
-              onChange={onDetailsChange}
-              value={userLoginDetails.username}
-              className='login-input' 
-              placeholder='Enter Username' />
-            <label>Password</label>
-            <input 
-              type="password" 
-              name="password" 
-              onChange={onDetailsChange}
-              value={userLoginDetails.password}
-              className='login-input' 
-              placeholder='Enter Password' />
-            <button className='login-button' type='submit'>
-                Login
-            </button>
-        </form>
-        <button className='login-register'>
-          <Link className='link' to="/register">
-            I'll Register
-          </Link>
-        </button>
-        {
-          error.error &&
-          <span style={{color: "red", marginTop: "10px"}}>{error.errorMessage}</span>
-        }
-    </div>
+    <div className='login-container'>
+      <div className='login'>
+          <span className='login-title'>Login</span>
+          <form className='login-form' onSubmit={onLoginSubmit}>
+              <label>Username</label>
+              <input 
+                type="text" 
+                name="username" 
+                onChange={onDetailsChange}
+                value={userLoginDetails.username}
+                className='login-input' 
+                placeholder='Enter Username' />
+              <label>Password</label>
+              <input 
+                type="password" 
+                name="password" 
+                onChange={onDetailsChange}
+                value={userLoginDetails.password}
+                className='login-input' 
+                placeholder='Enter Password' />
+              <button className='login-button' type='submit'>
+                  Login
+              </button>
+          </form>
+          <button className='login-register'>
+            <Link className='link' to="/register">
+              I'll Register
+            </Link>
+          </button>
+          {
+            error.error &&
+            <span style={{color: "red", marginTop: "10px"}}>{error.errorMessage}</span>
+          }
+      </div>
+    </div>  
   )
 }
--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -38,49 +38,51 @@ export default function Register() {
   }
 
   return (
-    <div className='register'>
-        <span className='register-title'>Register</span>
-        <form className='register-form' onSubmit={registerUser}>
-            <label>Username</label>
-            <input 
-              type="text" 
-              name='username' 
-              value={user.username}
-              onChange={userOnChange}
-              className='register-input' 
-              placeholder='Enter Username'
-            />
-            <label>Email</label>
-            <input 
-              type="email" 
-              name='email' 
-              value={user.email}
-              onChange={userOnChange}
-              className='register-input' 
-              placeholder='Enter Email' 
-            />
-            <label>Password</label>
-            <input 
-              type="password" 
-              name='password' 
-              value={user.password}
-              onChange={userOnChange}
-              className='register-input' 
-              placeholder='Enter Password' 
-            />
-            <button className='register-register' type='submit'>
-                Register
-            </button> 
-        </form>
-        <button className='login-button'>
-          <Link className='link' to="/login">
-            I'll login
-          </Link>
-        </button>
-        {
-          error &&
-          <span style={{color: "red", marginTop: "10px"}}>Something went wrong!</span>
-        }
+    <div className='register-container'>
+      <div className='register'>
+          <span className='register-title'>Register</span>
+          <form className='register-form' onSubmit={registerUser}>
+              <label>Username</label>
+              <input 
+                type="text" 
+                name='username' 
+                value={user.username}
+                onChange={userOnChange}
+                className='register-input' 
+                placeholder='Enter Username'
+              />
+              <label>Email</label>
+              <input 
+                type="email" 
+                name='email' 
+                value={user.email}
+                onChange={userOnChange}
+                className='register-input' 
+                placeholder='Enter Email' 
+              />
+              <label>Password</label>
+              <input 
+                type="password" 
+                name='password' 
+                value={user.password}
+                onChange={userOnChange}
+                className='register-input' 
+                placeholder='Enter Password' 
+              />
+              <button className='register-register' type='submit'>
+                  Register
+              </button> 
+          </form>
+          <button className='login-button'>
+            <Link className='link' to="/login">
+              I'll login
+            </Link>
+          </button>
+          {
+            error &&
+            <span style={{color: "red", marginTop: "10px"}}>Something went wrong!</span>
+          }
+      </div>
     </div>
   )
 }
